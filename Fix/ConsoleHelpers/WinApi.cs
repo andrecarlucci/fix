0667a356d86a6198ceba52b853d7b3e40e4eac19
andrecarlucci <andrecarlucci@gmail.com>
2021-12-31 11:21:23 +0100

    removed a ton of unused code

--- Fix/ConsoleHelpers/WinApi.cs
+++ Fix/ConsoleHelpers/WinApi.cs
@@ -28,8 +28,6 @@ namespace Mischel.ConsoleDotNet
 		{
 		}
 
-		// Some useful functions
-
 		[DllImport("kernel32.dll")]
 		public static extern bool CloseHandle(IntPtr handle);
 
@@ -39,16 +37,13 @@ namespace Mischel.ConsoleDotNet
 		public const int FILE_TYPE_PIPE = 0x0003;
 		public const int FILE_TYPE_REMOTE = 0x8000;
 
-		[DllImport("kernel32.dll")]
-		public static extern int GetFileType(IntPtr handle);
+        public const int CREATE_NEW = 1;
+        public const int CREATE_ALWAYS = 2;
+        public const int OPEN_EXISTING = 3;
+        public const int OPEN_ALWAYS = 4;
+        public const int TRUNCATE_EXISTING = 5;
 
-		public const int CREATE_NEW = 1;
-		public const int CREATE_ALWAYS = 2;
-		public const int OPEN_EXISTING = 3;
-		public const int OPEN_ALWAYS = 4;
-		public const int TRUNCATE_EXISTING = 5;
-
-		[DllImport("kernel32.dll", SetLastError = true)]
+        [DllImport("kernel32.dll", SetLastError = true)]
 		public static extern IntPtr CreateFile(
 			string Filename,
 			int DesiredAccess,
@@ -57,18 +52,5 @@ namespace Mischel.ConsoleDotNet
 			int CreationDisposition,
 			int FlagsAndAttributes,
 			IntPtr TemplateFile);
-
-		[DllImport("user32.dll", SetLastError = true)]
-		public static extern IntPtr FindWindow(
-			string lpClassName,
-			string lpWindowName);
-
-		[DllImport("user32.dll", SetLastError = true)]
-		public static extern int GetWindowThreadProcessId(
-		  IntPtr hWnd,
-		  ref int processId);
-
-		[DllImport("kernel32.dll", SetLastError = true)]
-		public static extern int GetCurrentProcessId();
 	}
 }
